1e06e0323c60775a014989d7b8a4b055ec967b0f
drumoff <41153738+drumoff@users.noreply.github.com>
2019-07-19 17:34:26 +0300

    Create createCalendar.js

---
+++ createCalendar.js
@@ -0,0 +1,59 @@
+function createCalendar(id, year, month){
+    'use strict';
+    
+    month = month - 1;
+    let date = new Date(year, month);
+    let firstDay = new Date(year, month, 1);
+    let lastDay = new Date(year, month + 1, 0);
+    let container = document.createElement('div');
+    
+    container.id = id;
+
+    firstDay = firstDay.getDay();
+    lastDay = lastDay.getDate();
+    console.log(date);
+    console.log(firstDay);
+    firstDay === 0 ? firstDay = 7 : firstDay = firstDay;
+    let daysWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+    let table = document.createElement('table');
+    table.innerHTML = '';
+    let thead = document.createElement('thead');
+    for(let i = 0; i < daysWeek.length; i++){
+        let th = document.createElement('th');
+        th.innerHTML = daysWeek[i];
+        thead.appendChild(th);
+    }
+
+    let cols = daysWeek.length;
+    let rows = 6;
+    let tbody = document.createElement('tbody');
+    //debugger;
+    for(let i = 0; i < rows; i++){
+        
+        let tr = document.createElement('tr');
+        for(let j = 0; j < cols; j++){
+            let td = document.createElement('td');
+            td.innerHTML = '';
+            tr.appendChild(td);
+
+        }
+        tbody.appendChild(tr);
+    }
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    container.appendChild(table);
+
+    document.body.appendChild(container);
+
+    let cells = document.querySelectorAll('td');
+    console.log(cells);
+    let startMonth = 1;
+    for(firstDay = firstDay - 1; firstDay < cells.length; firstDay++){
+        cells[firstDay].innerHTML = startMonth;
+        startMonth++;
+        if(startMonth > lastDay) {
+            break;
+        };
+    }
+};
